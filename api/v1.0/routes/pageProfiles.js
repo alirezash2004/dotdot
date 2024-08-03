c8658c8f57bea9827233e8008b9f913b093fc078
alirezash2004 <alirezashabany2004@gmail.com>
2024-08-03 11:22:57 +0330

    remove deletepageprofile(not needed)

--- api/v1.0/routes/pageProfiles.js
+++ api/v1.0/routes/pageProfiles.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { deletePageProfile, getPageProfile, updatePageProfile } from '../controllers/pageProfileController.js';
+import { getPageProfile, updatePageProfile } from '../controllers/pageProfileController.js';
 import { checkSchema } from 'express-validator';
 import { updateProfileSchema } from '../validators/schemas/pageProfileSchema.js';
 const router = Router();
@@ -23,15 +23,5 @@ router.post('/pageProfile/:pageId', checkSchema({
         trim: true
     }
 }, ['params']), checkSchema(updateProfileSchema), updatePageProfile);
-
-router.delete('/pageProfile/:pageId', checkSchema({
-    pageId: {
-        exists: {
-            errorMessage: 'pageId is required'
-        },
-        isEmpty: { negated: true },
-        trim: true
-    }
-}, ['params']), deletePageProfile);
 
 export default router;
