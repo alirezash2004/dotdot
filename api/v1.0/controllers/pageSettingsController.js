07d7d610d1a6178facc93f60b01f7b7bb188377e
alirezash2004 <alirezashabany2004@gmail.com>
2024-08-03 12:04:01 +0330

    connect `pagesettings ` to `db`

--- api/v1.0/controllers/pageSettingsController.js
+++ api/v1.0/controllers/pageSettingsController.js
@@ -1,7 +1,9 @@
 import { matchedData, validationResult } from "express-validator";
 import { samplepageSettings } from "../helpers/mockuser.js";
+import { Page, PageSetting } from "../mongoose/schemas/page.js";
+import mongoose from "mongoose";
 
-export const getpageSettings = (req, res, next) => {
+export const getpageSettings = async (req, res, next) => {
     // TODO: check if page is public -> return bio 
     // if not public -> check if is follower
 
@@ -13,23 +15,26 @@ export const getpageSettings = (req, res, next) => {
     const data = matchedData(req);
 
     const pageId = data.pageId;
-    if (!pageId) {
-        const error = new Error(`An Error Accrued While Fetching Page Username!`);
-        error.status = 500;
-        return next(error);
+
+    const isValidId = mongoose.Types.ObjectId.isValid(pageId);
+
+    if (!isValidId) {
+        const err = new Error(`pageId is not valid!`);
+        err.status = 400;
+        return next(err);
     }
 
-    const pageSettings = samplepageSettings.find(pgprofile => pgprofile.pageId === pageId);
-    if (!pageSettings) {
-        const error = new Error(`Page with pageid '${pageId}' has no pageSettings!`);
-        error.status = 500;
-        return next(error);
+    const pagesetting = await Page.findById(pageId).select('pageSetting').populate('pageSetting').exec()
+    if (!pagesetting) {
+        const err = new Error(`A pagesetting with pageid '${pageId}' was not found!`);
+        err.status = 404;
+        return next(err);
     }
 
-    res.status(200).json({ msg: 'success', pageSettings });
+    res.status(200).json({ msg: 'success', pageSetting: pagesetting.pageSetting });
 }
 
-export const updatepageSettings = (req, res, next) => {
+export const updatepageSettings = async (req, res, next) => {
     const result = validationResult(req).array({ onlyFirstError: true });
     if (result.length !== 0) {
         return res.status(400).send({ msg: result[0].msg });
@@ -38,24 +43,33 @@ export const updatepageSettings = (req, res, next) => {
     const data = matchedData(req);
 
     const pageId = data.pageId;
-    if (!pageId) {
-        const error = new Error(`An Error Accrued While Fetching Page Username!`);
-        error.status = 500;
-        return next(error);
-    }
 
-    const pageSettings = samplepageSettings.find(pageSettings => pageSettings.pageId === pageId);
-    if (!pageSettings) {
-        const error = new Error(`Page with pageid '${pageId}' has no pageSettings!`);
-        error.status = 500;
-        return next(error);
+    const isValidId = mongoose.Types.ObjectId.isValid(pageId);
+
+    if (!isValidId) {
+        const err = new Error(`pageId is not valid!`);
+        err.status = 400;
+        return next(err);
     }
 
     const { theme, language, country } = data;
 
-    pageSettings.theme = theme;
-    pageSettings.language = language;
-    pageSettings.country = country;
+    const page = await Page.findById(pageId).select('pageSetting').populate('pageSetting').exec();
+    if (!page) {
+        const err = new Error(`A pagesetting with pageid '${pageId}' was not found!`);
+        err.status = 404;
+        return next(err);
+    }
 
-    res.status(200).json({ msg: 'pageSettings updated', pageSettings });
+    const pagesettingId = page.pageSetting._id.toString();
+
+    const queryResult = await PageSetting.updateOne({ _id: pagesettingId }, { theme, language, country });
+
+    if (!queryResult.acknowledged) {
+        const err = new Error(`Something went wrong updating pageSetting!`);
+        err.status = 500;
+        return next(err);
+    }
+
+    res.status(200).json({ msg: 'pageSettings updated' });
 }
