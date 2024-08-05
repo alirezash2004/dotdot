ca3c3b19a8c59b981e854d1e84291aecf8601c43
alirezash2004 <alirezashabany2004@gmail.com>
2024-08-05 12:16:23 +0330

    add `auth` to `pageSettings`
    + remove pageId parameter

--- api/v1.0/controllers/pageSettingsController.js
+++ api/v1.0/controllers/pageSettingsController.js
@@ -2,20 +2,7 @@ import { Page, PageSetting } from "../mongoose/schemas/page.js";
 import { isValidObjectId } from 'mongoose';
 
 export const getpageSettings = async (req, res, next) => {
-    // TODO: check if page is public -> return bio 
-    // if not public -> check if is follower
-
-    const data = req.validatedData;
-
-    const pageId = data.pageId;
-
-    const isValidId = isValidObjectId(pageId);
-
-    if (!isValidId) {
-        const err = new Error(`pageId is not valid!`);
-        err.status = 400;
-        return next(err);
-    }
+    const pageId = req.user._id;
 
     const pagesetting = await Page.findById(pageId).select('pageSetting').populate('pageSetting').exec()
     if (!pagesetting) {
@@ -24,30 +11,15 @@ export const getpageSettings = async (req, res, next) => {
         return next(err);
     }
 
-    res.status(200).json({ msg: 'success', pageSetting: pagesetting.pageSetting });
+    res.status(200).json({ msg: 'success', data: pagesetting.pageSetting });
 }
 
 export const updatepageSettings = async (req, res, next) => {
-    const data = req.validatedData;
-
-    const pageId = data.pageId;
-
-    const isValidId = isValidObjectId(pageId);
-
-    if (!isValidId) {
-        const err = new Error(`pageId is not valid!`);
-        err.status = 400;
-        return next(err);
-    }
+    const pageId = req.user._id;
 
     const { theme, language, country } = data;
 
     const page = await Page.findById(pageId).select('pageSetting').populate('pageSetting').exec();
-    if (!page) {
-        const err = new Error(`A pagesetting with pageid '${pageId}' was not found!`);
-        err.status = 404;
-        return next(err);
-    }
 
     const pagesettingId = page.pageSetting._id.toString();
 
--- api/v1.0/routes/pageSettings.js
+++ api/v1.0/routes/pageSettings.js
@@ -3,11 +3,11 @@ import { getpageSettings, updatepageSettings } from '../controllers/pageSettings
 import { checkSchema } from 'express-validator';
 import { updatePageSettingSchema } from '../validators/schemas/pageSettingSchema.js';
 import validationResultHandler from '../middleware/validationResultHandler.js';
-import pageIdSchema from '../validators/schemas/pageIdSchema.js';
+import passport from 'passport';
 const router = Router();
 
-router.get('/pageSettings/:pageId', checkSchema(pageIdSchema, ['params']), validationResultHandler, getpageSettings);
+router.get('/pageSettings', passport.authenticate('jwt', { session: false }), getpageSettings);
 
-router.post('/pageSettings/:pageId', checkSchema(pageIdSchema, ['params']), checkSchema(updatePageSettingSchema), validationResultHandler, updatepageSettings);
+router.post('/pageSettings', passport.authenticate('jwt', { session: false }), checkSchema(updatePageSettingSchema), validationResultHandler, updatepageSettings);
 
 export default router;
