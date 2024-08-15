858b977155c25420608c1807e72a7d88153e82a0
AlirezaSh2004 <alirezashabany2004@gmail.com>
2024-08-15 13:07:32 +0330

    add TanStack Query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClinet, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClinet = new QueryClinet();
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
+			<QueryClientProvider client={queryClinet}>
+				<App />
+			</QueryClientProvider>
 		</BrowserRouter>
 	</StrictMode>
 );
