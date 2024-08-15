a41f83342536ced6b1c4e4f52ae7063023fff207
AlirezaSh2004 <alirezashabany2004@gmail.com>
2024-08-15 13:15:03 +0330

    minor typo fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClinet, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClinet = new QueryClinet();
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
-			<QueryClientProvider client={queryClinet}>
+			<QueryClientProvider client={queryClient}>
 				<App />
 			</QueryClientProvider>
 		</BrowserRouter>
