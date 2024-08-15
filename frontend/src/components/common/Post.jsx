1d5c98885f6b5c28b712fe7207771e2d2d475d08
AlirezaSh2004 <alirezashabany2004@gmail.com>
2024-08-15 13:06:45 +0330

    add post boilerplate

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -1,7 +1,38 @@
-const Post = () => {
-  return (
-    <div>Post</div>
-  )
-}
+import { useState } from "react";
 
-export default Post
+const Post = ({ post }) => {
+	const [comment, setComment] = useState("");
+	const postSender = post.page;
+	const isLiked = false;
+
+	const isMyPost = true;
+
+	const formattedDate = "1h";
+
+	const isCommenting = false;
+
+	const handleDeletePost = () => {};
+
+	const handlePostComment = (e) => {
+		e.preventDefault();
+	};
+
+	const handleLikePost = () => {};
+
+	return (
+		<>
+			<div className="flex w-96 flex-col gap-4 max-w-56 mx-auto md:max-w-full">
+				<div className="skeleton h-64 w-full"></div>
+				<div className="flex gap-2">
+					<div className="skeleton h-4 w-5"></div>
+					<div className="skeleton h-4 w-5"></div>
+					<div className="skeleton h-4 w-5"></div>
+				</div>
+				<div className="skeleton h-4 w-full"></div>
+				<div className="skeleton h-4 w-full"></div>
+			</div>
+		</>
+	);
+};
+
+export default Post;
--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -18,7 +18,7 @@ const Posts = () => {
 				<p className="text-center my-4">OOPS! No Posts Found!</p>
 			)}
 			{!isLoading && POSTS && (
-				<div>
+				<div className="flex flex-col gap-16 mx-auto my-10 md:my-24">
 					{POSTS.map((post) => (
 						<Post key={post._id} post={post} />
 					))}
