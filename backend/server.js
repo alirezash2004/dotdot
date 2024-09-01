6ae71d1b5cc956b005999097c4bedc92259b6579
AlirezaSh2004 <alirezashabany2004@gmail.com>
2024-09-01 15:14:24 +0330

    minor typo fix

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.use('/api/v1.0', v1_0);
 // if (process.env.NODE_ENV === "production") {
 //     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-//     app.get("*", (res, res) => {
+//     app.get("*", (req, res) => {
 //         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 //     })
 // }
