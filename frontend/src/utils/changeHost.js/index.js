837128a75bf380d291346fde6d08ba828f2d9420
AlirezaSh2004 <alirezashabany2004@gmail.com>
2024-09-13 18:59:17 +0330

    frontend -- remove .webp exp from assets

--- frontend/src/utils/changeHost.js/index.js
+++ frontend/src/utils/changeHost.js/index.js
@@ -1,6 +1,6 @@
 // TODO: fix this (use real host when uploading)
 const changeHost = (str) => {
-    return str.replace("localhost", "10.61.18.10");
+    return str.replace("localhost", "10.61.18.10").replace(".webp", "");
 }
 
 export default changeHost;
