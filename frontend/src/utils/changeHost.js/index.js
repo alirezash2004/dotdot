9b7671995c066898534f6dfcd44c833e64167f07
AlirezaSh2004 <alirezashabany2004@gmail.com>
2024-10-02 10:14:55 +0330

    add deploy file(loader)

--- frontend/src/utils/changeHost.js/index.js
+++ frontend/src/utils/changeHost.js/index.js
@@ -1,6 +1,7 @@
 // TODO: fix this (use real host when uploading)
 const changeHost = (str) => {
     return str.replace("localhost", "10.61.18.10").replace(".webp", "");
+    // return str.replace("localhost", "--WEBSITEURL--").replace("http", "https").replace(".webp", "");
 }
 
 export default changeHost;
